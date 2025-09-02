d56d34d75c10428b339e132db57aff3b055bb922
DaphneLoustalet <114203018+DaphneLoustalet@users.noreply.github.com>
2025-09-02 09:32:14 -0500

    more updates to game logic

--- hangman.js
+++ hangman.js
@@ -22,16 +22,85 @@ async function fetchAndShowRandomWord() {
 */
 
 function activateGamePlay(word) {
-  console.log("Word is ", word)
+  const unguessed_letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i));
+  let hangmanPoints = 0; /* Track which part of the hangman needs to be shown */
+  cleanBoard();
   generateSpaces(word);
   document.addEventListener("keydown", function(event) {
     if (/^[a-zA-Z]$/.test(event.key)) {
       // it is a letter
       if (word.includes(event.key)) {
-        console.log("Letter in word");
+        showUserCorrectLetters(event.key, word, unguessed_letters);
       } else {
-        console.log("Letter not in word");
+        logIncorrectLetter(event.key, unguessed_letters);
+        hangMan(hangmanPoints);
       }
+    }
+  });
+}
+
+function hangMan(hangmanPoints) {
+  hangmanPoints++;
+  if (hangmanPoints == 1) {
+    show('.head');
+  } else if (hangmanPoints == 2) {
+    show('.body');
+  } else if (hangmanPoints == 3) {
+    show('arm left');
+  } else if (hangmanPoints == 4) {
+    show('arm right');
+  } else if (hangmanPoints == 5) {
+    show('leg left');
+  } else {
+    show('leg right');
+    endGame();
+  }
+}
+
+function show(classname) {
+  let element = document.querySelector(classname);
+  element.style.display = 'block';
+}
+
+function endGame() {
+  console.log("You lose!");
+}
+
+function logIncorrectLetter(guess, remaining_guesses) {
+  const incorrectletterbox = document.querySelector('.missed-letters');
+  incorrectletterbox.textContent = guess;
+  removeGuess(remaining_guesses, guess);
+}
+
+function showUserCorrectLetters(guess, word, remaining_guesses) {
+  const spans = document.querySelectorAll('#underscore-container span');
+  for (let i = 0; i < word.length; i++) {
+    if (guess == word[i]) {
+      spans[i].textContent = guess;
+    }
+  }
+  removeGuess(remaining_guesses, guess);
+}
+
+function removeGuess(remaining_guesses, guess) {
+  for (let i = 0; i < remaining_guesses.length; i++) {
+    if (remaining_guesses[i] === guess) {
+      remaining_guesses.splice(i, 1);
+      break; 
+    }
+  }
+}
+
+function cleanBoard() {
+  const container = document.getElementById('underscore-container');
+
+  // Get all span elements inside the container
+  const spans = container.querySelectorAll('span');
+
+  // Loop through and remove spans that contain only "_"
+  spans.forEach(span => {
+    if (span.textContent.trim() === '_') {
+      span.remove();
     }
   });
 }
